db8bdb2f493b41dd9f0d3ec1a504e5bad421a984
cjp-engr <67772382+cjp-engr@users.noreply.github.com>
2022-05-06 00:23:27 -0700

    view hour ui design update

--- lib/widgets/employees_page/employee_view_hours_dialog.dart
+++ lib/widgets/employees_page/employee_view_hours_dialog.dart
@@ -6,84 +6,69 @@ class EmployeeViewHoursDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      child: SingleChildScrollView(
-        child: Container(
-          width: MediaQuery.of(context).size.width / 1.4,
-          height: MediaQuery.of(context).size.height,
-          decoration: const BoxDecoration(
-            color: Colors.white,
-          ),
-          child: Column(
-            children: [
-              Text(
-                'TOTAL WORK TIME',
-                style: Theme.of(context).textTheme.headline5!.merge(
-                      const TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
+      child: Container(
+        width: MediaQuery.of(context).size.width / 1.4,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Column(
+          children: [
+            Text(
+              'TOTAL WORK TIME',
+              style: Theme.of(context).textTheme.headline5!.merge(
+                    const TextStyle(
+                      fontWeight: FontWeight.bold,
                     ),
+                  ),
+            ),
+            Text(
+              '00:00',
+              style: Theme.of(context).textTheme.headline5!.merge(
+                    const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 50,
               ),
-              Text(
-                '00:00',
-                style: Theme.of(context).textTheme.headline5!.merge(
-                      const TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width / 6.5,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
                     ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 6.5,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 5.5,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.delete_outline,
+                      size: 45,
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 50,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 5.5,
-                      height: 50,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: const Text('start'),
-                        style: ElevatedButton.styleFrom(
-                          side: const BorderSide(
-                            width: 1.0,
-                            color: Colors.black,
-                          ),
-                          primary: Colors.white,
-                          onPrimary: Colors.black,
-                          elevation: 0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 5.5,
-                      height: 50,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: const Text('end'),
-                        style: ElevatedButton.styleFrom(
-                          side: const BorderSide(
-                            width: 1.0,
-                            color: Colors.black,
-                          ),
-                          primary: Colors.white,
-                          onPrimary: Colors.black,
-                          elevation: 0,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width / 5.5,
-                      height: 50,
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
